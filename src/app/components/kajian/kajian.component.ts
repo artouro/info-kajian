6b76ea51a9fb8c5d27a71d4de7f5dd32f4298970
artour <sir.louis.trenet@gmail.com>
2018-11-06 23:26:18 +0700

    Fix homepage rusak

--- src/app/components/kajian/kajian.component.ts
+++ src/app/components/kajian/kajian.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { KajianService } from '../../services/kajian/kajian.service';
 import { Kajian } from '../../models/Kajian';
+
+declare var Flickity: any;
 
 @Component({
   selector: 'app-kajian',
   templateUrl: './kajian.component.html',
   styleUrls: ['./kajian.component.css']
 })
-export class KajianComponent implements OnInit {
+export class KajianComponent implements OnInit, AfterViewInit {
   kajian: Kajian[];
     constructor(private kajianService: KajianService) {
   }
